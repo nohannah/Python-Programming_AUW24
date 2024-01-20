89d8b2ceca2d34b41c34163f0a7aaabd99d89a70
nohannah <nohannah508@gmail.com>
2024-01-20 22:52:35 +0600

    using one star

---
+++ practice1.py
@@ -0,0 +1,16 @@
+# #1st problem
+# print('*'*5)
+# print("*   " *2)
+# print('*'*5)
+#  #2nd problem 
+# print('*'*5)
+# print('*'*5)
+# print('*'*5)
+# print('*'*5)
+# print('*'*5)
+# #one line 
+# print((('***** \n'))*5)
+#using one * 
+X=("*" * 5 +'\n') #\n is to enter the line but you need to put inside the bracket 
+Y=(X * 5)
+print(Y)
