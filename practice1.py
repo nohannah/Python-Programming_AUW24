f705640e62e30ca46844f414d570bf98c33ccc75
nohannah <nohannah508@gmail.com>
2024-01-28 22:43:39 +0600

    28th

--- practice1.py
+++ practice1.py
@@ -9,8 +9,21 @@
 # print('*'*5)
 # print('*'*5)
 # #one line 
+# print("*****\n"+"*****"+"")
 # print((('***** \n'))*5)
 #using one * 
-X=("*" * 5 +'\n') #\n is to enter the line but you need to put inside the bracket 
-Y=(X * 5)
-print(Y)
+#print((("*"*5 )+"\n" )*5)
+# X=("*" * 5 +'\n') #\n is to enter the line but you need to put inside the bracket 
+# Y=(X * 5)
+# print(Y)
+# h=3
+# b=5
+# print(h*b/2)
+# #practice2 
+# num=int(input("Enter the number:"))
+# word=str(input("Enter the word:"))
+# print((num*word))
+
+# Pracrice4
+phrase="Pony stach token"
+phrase[5:9]
---
+++ practice4.py
@@ -0,0 +1,14 @@
+phrase= "Pony stash token"
+#print the string slice "stash"
+print(phrase[5:10])
+
+# print the string slice"
+print(phrase[-16:-12])
+
+#print backward
+print(phrase[::-1])
+
+#print python
+print(phrase[:1]+ phrase[3:4] + phrase[6:7]+phrase[12:13]+phrase[15:])
+
+
