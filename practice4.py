43734b22ff2c3c2b8bb8f8563933eabcfd9706cf
nohannah <nohannah508@gmail.com>
2024-01-31 13:08:18 +0600

    len

--- practice4.py
+++ practice4.py
@@ -1,14 +1,25 @@
-phrase= "Pony stash token"
-#print the string slice "stash"
-print(phrase[5:10])
+# phrase= "Pony stash token"
+# #print the string slice "stash"
+# print(phrase[5:10])
 
-# print the string slice"
-print(phrase[-16:-12])
+# # print the string slice"
+# print(phrase[-16:-12])
 
-#print backward
-print(phrase[::-1])
+# #print backward
+# print(phrase[::-1])
 
-#print python
-print(phrase[:1]+ phrase[3:4] + phrase[6:7]+phrase[12:13]+phrase[15:])
-
-
+# #print python
+# print(phrase[:1]+ phrase[3:4] + phrase[6:7]+phrase[9:10]+ phrase[12:13]+phrase[15:])
+# #another type
+# print(phrase[0::3])
+#print(phrase[::3])
+#print(phrase[0:16:3])
+# phrase="Today is raining"
+# print(phrase[9:7:-1])
+#print the last character of the words
+word=input("Enter any words:")
+print(word[-1:])
+#without using the negative
+lenght=len(word)
+print(word[lenght-1])
+print(word[len(word)-1])
